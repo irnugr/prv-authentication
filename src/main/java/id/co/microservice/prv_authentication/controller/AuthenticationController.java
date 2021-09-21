5f682095f5ff1d4d296049993f86a9d5af63342a
irnugr <irfan.dwiaji@gmail.com>
2021-09-21 23:06:30 +0700

    Add request mapping url

--- src/main/java/id/co/microservice/prv_authentication/controller/AuthenticationController.java
+++ src/main/java/id/co/microservice/prv_authentication/controller/AuthenticationController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import id.co.microservice.prv_authentication.dao.UserLoginRequest;
@@ -13,6 +14,7 @@ import id.co.microservice.prv_authentication.service.AuthenticationService;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/api/v1/")
 public class AuthenticationController {
 	
 	@Autowired
